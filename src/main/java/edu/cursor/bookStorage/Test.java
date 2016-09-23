bdedf8d1841d1174670c9be45a4b6bfc2767dc9c
RootUA <pavelkevich@gmail.com>
2016-09-23 20:32:15 +0300

    small correction in names

--- src/main/java/edu/cursor/bookStorage/Test.java
+++ src/main/java/edu/cursor/bookStorage/Test.java
@@ -7,30 +7,29 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
-        BookManager cm = new BookManager();
+        BookManager bm = new BookManager();
 
         Book c1 = new Book("Socrat", "Book1", 10);
         Book c2 = new Book("Shakespeare", "Book2", 20);
         Book c3 = new Book("King", "Book3", 30);
-
         System.out.println("ADD Book ==============");
-        Integer cId1 = cm.addBook(c1);
-        Integer cId2 = cm.addBook(c2);
-        Integer cId3 = cm.addBook(c3);
-        List<Book> result1 = cm.findBooks();
-        for(Book c : result1) {
-            System.out.println(c);
+        Integer ISBN1 = bm.addBook(c1);
+        Integer ISBN2 = bm.addBook(c2);
+        Integer ISBN3 = bm.addBook(c3);
+        List<Book> result1 = bm.findBooks();
+        for(Book b : result1) {
+            System.out.println(b);
         }
 
         System.out.println("DELETE Book ==============");
-        cm.deleteBook(cId1);
-        List<Book> result3 = cm.findBooks();
-        for(Book c : result3) {
-            System.out.println(c);
+        bm.deleteBook(ISBN1);
+        List<Book> result3 = bm.findBooks();
+        for(Book b : result3) {
+            System.out.println(b);
         }
 
         System.out.println("GET Book ==============");
-        Book Book = cm.getBook(cId2);
+        Book Book = bm.getBook(ISBN2);
         System.out.println(Book);
     }
 }
