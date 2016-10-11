d2c786bbebb7fb979f4459a2b7291493088123e9
Olesya <oleskafeska@gmail.com>
2016-10-11 17:09:31 +0300

    CRSRL0-1:created file TblUser.java/ add functionality

---
+++ src/main/java/edu/cursor/library/user/entity/TblUser.java
@@ -0,0 +1,143 @@
+package edu.cursor.library.user.entity;
+
+import java.util.Date;
+
+public class TblUser {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String eMail;
+	private int mobileNum;
+	private String address;
+	private Date dateOfRegistration;
+	
+	public TblUser(int id, String firstName, String lastName, String eMail, int mobileNum, String address,
+			Date dateOfRegistration) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.eMail = eMail;
+		this.mobileNum = mobileNum;
+		this.address = address;
+		this.dateOfRegistration = dateOfRegistration;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public int getMobileNum() {
+		return mobileNum;
+	}
+
+	public void setMobileNum(int mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateOfRegistration() {
+		return dateOfRegistration;
+	}
+
+	public void setDateOfRegistration(Date dateOfRegistration) {
+		this.dateOfRegistration = dateOfRegistration;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((dateOfRegistration == null) ? 0 : dateOfRegistration.hashCode());
+		result = prime * result + ((eMail == null) ? 0 : eMail.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + mobileNum;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TblUser other = (TblUser) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (dateOfRegistration == null) {
+			if (other.dateOfRegistration != null)
+				return false;
+		} else if (!dateOfRegistration.equals(other.dateOfRegistration))
+			return false;
+		if (eMail == null) {
+			if (other.eMail != null)
+				return false;
+		} else if (!eMail.equals(other.eMail))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (mobileNum != other.mobileNum)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TblUser [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", eMail=" + eMail
+				+ ", mobileNum=" + mobileNum + ", address=" + address + ", dateOfRegistration=" + dateOfRegistration
+				+ "]";
+	}
+	
+	}
