31e7d5a4ca254427b5ea4fdd86603820af9ac36c
bwstudio <bwstudio@ukr.net>
2016-11-04 20:08:38 +0200

    Last Library structure .sql

--- Library-script.sql
+++ Library-script.sql
@@ -23,6 +23,11 @@ dateOfRegistration date,
 role varchar (45)
 );
 
+create table credentials
+(id int primary key auto_increment,
+pass varchar (255)
+);
+
 create table registry
 (user_id int primary key auto_increment,
 book_id int
