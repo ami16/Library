7baf521f5bbd95c64bee066a1293059063d8fe60
bwstudio <bwstudio@ukr.net>
2016-11-11 13:12:44 +0200

    sql script~

--- Library-db-script.sql
+++ Library-db-script.sql
@@ -1,9 +1,9 @@
-drop database if exists Library;
-create database Library;
-use Library;
+drop database if exists ownlib;
+create database ownlib;
+use ownlib;
 
 create table books
-(id_book int primary key auto_increment,
+(id int primary key auto_increment,
 ISBN varchar (25),
 author varchar (45),
 title varchar (45),
@@ -13,7 +13,7 @@ genre varchar (45)
 );
 
 create table users
-(id_user int primary key auto_increment,
+(id int primary key auto_increment,
 firstName varchar (45), 
 lastName varchar (45),
 eMail varchar (45),
